c4a6af847566a9e5719175b6eddc83408d87d1ae
Josh Allen <axlecrusher@gmail.com>
2017-07-08 14:15:10 -0400

    add hgstring type

--- Mercury3/str_utils.c
+++ Mercury3/str_utils.c
@@ -4,6 +4,21 @@
 #include <stdlib.h>
 #include <assert.h>
 
+uint32_t hgstring_append(hgstring* hg, const char* str) {
+	uint32_t slen = strlen(str) + 1; //include null
+	if ((hg->used + slen) > hg->allocated_len) {
+		char * s = realloc(hg->str, hg->allocated_len + 128);
+		assert(s != NULL);
+		hg->str = s;
+		hg->allocated_len += 128;
+	}
+
+	memcpy(hg->str + hg->used, str, slen); //includes null
+	uint32_t offset = hg->used;
+	hg->used += slen;
+	return offset;
+}
+
 char* str_cat(const char* s1, const char* s2) {
 	uint32_t size = strlen(s1) + strlen(s2);
 	char* str = malloc(size + 1);
--- Mercury3/str_utils.h
+++ Mercury3/str_utils.h
@@ -1,4 +1,15 @@
 #pragma once
 
+#include <stdint.h>
+
+typedef struct hgstring {
+	char* str;
+	uint32_t allocated_len;
+	uint32_t used;
+} hgstring;
+
+uint32_t hgstring_append(hgstring* hg, const char* str);
+
+
 char* str_cat(const char* s1, const char* s2);
 char* str_copy(const char* str);
