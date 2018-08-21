8050a389621e8b3da024cbdb629237fee67aaae6
Josh Allen <josh@allensw.com>
2018-08-21 05:56:18 -0400

    fix dual quaternion multiplication

--- Mercury3/quaternion_dual.cpp
+++ Mercury3/quaternion_dual.cpp
@@ -4,8 +4,7 @@ dual_quaternion::dual_quaternion(const quaternion& rotation, const vector3& tran
 	: m_real(rotation.normal())
 {
 	//result must not be normalized
-	const auto tmp = quaternion::mult(quaternion(0, translation.x(), translation.y(), translation.z()), m_real);
-	m_dual = tmp.scale(0.5f);
+	m_dual = quaternion::mult(quaternion(0, translation.x(), translation.y(), translation.z()), m_real).scale(0.5f);
 }
 
 dual_quaternion dual_quaternion::scale(float m) const {
@@ -43,16 +42,16 @@ dual_quaternion dual_quaternion::operator-(const dual_quaternion& rhs) const {
 dual_quaternion dual_quaternion::operator*(const dual_quaternion& rhs) const {
 	//left to right order
 	dual_quaternion r;
-	r.m_real = m_real * rhs.m_real;
-	//I don't know if this needs to be normalized or not
-	r.m_dual = (m_real * rhs.m_dual + m_dual * rhs.m_real).scale(0.5); //this is exactly 2x. why?
-	//r.m_dual = (quaternion::quat_mult(m_real, rhs.m_dual) + quaternion::quat_mult(rhs.m_dual, m_real));// .scale(0.5);
-	return r;
+	r.m_real = quaternion::mult(m_real, rhs.m_real);
+	//The equations below seems correct.
+	r.m_dual = (quaternion::mult(m_real, rhs.m_dual) + quaternion::mult(m_dual, rhs.m_real));
+	return r.normal();
 }
 
 HgMath::vec3f dual_quaternion::getTranslation() const {
+vertex3f dual_quaternion::getTranslation() const {
 	using namespace HgMath;
-	vec3f r;
+	vertex3f r;
 
 	//result must not be normalized
 	quaternion tmp = quaternion::mult(m_dual.scale(2.0), m_real.conjugate());
